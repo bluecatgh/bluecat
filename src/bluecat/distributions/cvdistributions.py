67bf7f60c6ee30c798f66906f5a94990d90b8ef3
bluecatgh <bluecat5452@gmail.com>
2026-09-25 12:30:35 -0500

    Add exponential distribution function for HW02

--- src/bluecat/distributions/cvdistributions.py
+++ src/bluecat/distributions/cvdistributions.py
@@ -1,4 +1,5 @@
 import secrets
+import math
 
 
 def uniform(a: float = 0.0, b: float = 1.0) -> float:
@@ -6,3 +7,17 @@ def uniform(a: float = 0.0, b: float = 1.0) -> float:
     # 53 random bits gives 53-bit precision double
     u = secrets.randbits(53) / (1 << 53)  # in [0, 1)
     return a + (b - a) * u
+
+
+def exponentialdist(lam: float) -> float:
+    """Cryptographically secure exponentially distributed sample."""
+    if lam <= 0:
+        raise ValueError("lambda must be greater than 0")
+
+    y = uniform()
+
+    # Avoid log(0)
+    while y == 0.0:
+        y = uniform()
+
+    return -(1.0 / lam) * math.log(y)
